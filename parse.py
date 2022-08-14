a44b4f912326d2e113d2caaa422284f775dc7a0b
marydrobotun <marydrobotun@gmail.com>
2022-08-15 02:14:05 +0300

    less logs

--- parse.py
+++ parse.py
@@ -10,7 +10,7 @@ def parse(coordinates):
     request = 'https://api.openweathermap.org/data/2.5/weather?lat=' \
               + str(coordinates['lat']) + '&lon=' + str(coordinates['lon']) + '&appid=' + weather_key + '&units=metric'
     weather = requests.get(request)
-    pprint.pprint(json.loads(weather.text))
+#    pprint.pprint(json.loads(weather.text))
     json_weather = json.loads(weather.text)
     return json_weather
 
@@ -22,18 +22,8 @@ def get_coordinates(city):
     json_result = json.loads(result.text)
     if len(json_result) == 0:
         return False
-    pprint.pprint(json_result)
+#    pprint.pprint(json_result)
     result = {}
     result['lat'] = json_result[0]['lat']
     result['lon'] = json_result[0]['lon']
     return result
-
-
-
-
-
-
-
-if __name__=='__main__':
-    get_coordinates('Москва')
-    print(emoji.emojize('Python is :thumbs_up:'))
